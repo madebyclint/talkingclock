57ad1315405745644326c5536b7cd5fdd3cc8927
Clint Bush <clint@madebyclint.com>
2016-11-06 21:48:26 -0800

    Add alarm basic functionality.

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,28 @@ var Clock = (function() {
         secondDisplay = document.getElementById('second'),
         initTime = new Date(),
         initHour = initTime.getHours(),
-        initMinute = initTime.getMinutes();
+        initMinute = initTime.getMinutes(),
+        alarmTriggered = false;
     var checkMinute = function(min) {
         if (initMinute < min) {
             console.log('hello new minute', min);
             responsiveVoice.speak('The time is now ' + initHour + ':' + min);
             initMinute = min;
+        }
+    };
+    var checkAlarm = function(hr, min) {
+        if (alarmTriggered) {
+            // checkMinute(min);
+            console.log('nope', alarmTriggered);
+            console.log(hr, min);
+        } else {
+            console.log('yep', alarmTriggered);
+            console.log(hr, min);
+            if (hr === 21 && min === 48) {
+                alarmTriggered = true;
+                responsiveVoice.speak('Good morning. Time to wake up. The time is now ' + initHour + ':' + min);
+                console.log('Good morning');
+            }
         }
     };
     var timer = setInterval(function() {
@@ -21,6 +37,6 @@ var Clock = (function() {
         hourDisplay.innerHTML = h;
         minuteDisplay.innerHTML = m;
         secondDisplay.innerHTML = s;
-        checkMinute(m);
+        checkAlarm(h, m);
     }, 500);
 })();
