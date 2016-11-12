d80d7787c26fd97b697eaad9f4dcab7b8f7ee631
Clint Bush <clint@madebyclint.com>
2016-11-11 22:02:54 -0800

    Get basic react app structure setup.

--- scripts/reactapp.js
+++ scripts/reactapp.js
@@ -1,3 +1,33 @@
 // From https://github.com/ForbesLindesay/tutorials-for-building-real-apps/tree/master/04-react-clock
 
 
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var container = document.getElementById('clockcontainer');
+
+ReactDOM.render(
+    React.createElement(
+        'p',
+        {},
+        'This is a paragraph of text with ',
+        React.createElement(
+            'a',
+            {href: 'http://www.example.com'},
+            'links to other web sites'
+        ),
+        ', ',
+        React.createElement(
+            'b',
+            {},
+            'some text in bold'
+        ),
+        ' and ',
+        React.createElement(
+            'i',
+            {},
+            'some text in italics'
+        )
+    ),
+    container
+)
