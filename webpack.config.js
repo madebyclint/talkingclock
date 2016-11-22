08329942ef35fd5501af70c6449af1d9eb67add0
Clint Bush <clint@madebyclint.com>
2016-11-22 15:55:46 -0800

    Add babel loader to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ var APP_DIR = path.resolve(__dirname, 'scripts/reactapp');
 
 var config = {
   entry: APP_DIR + '/reactapp.jsx',
+  module: {
+    loaders: [
+        {
+            test: /\.jsx?/,
+            include: APP_DIR,
+            loader: 'babel'
+        }
+    ]
+  },
   output: {
     path: BUILD_DIR,
     filename: 'reactbundle.js'
