d29d0584ca8e11e8ccbddb48751732538b2f9e09
Clint Bush <clint@madebyclint.com>
2016-11-30 20:48:04 -0800

    Clean up commented code.

--- scripts/reactapp/reactapp.jsx
+++ scripts/reactapp/reactapp.jsx
@@ -1,18 +1,7 @@
 // From https://github.com/ForbesLindesay/tutorials-for-building-real-apps/tree/master/04-react-clock
 
-
-// var React = require('react');
-// var ReactDOM = require('react-dom');
 import React from 'react';
 import {render} from 'react-dom';
-
-// class App extends React.Component {
-//     render () {
-//         return <p> Hello React! </p>;
-//     }
-// }
-
-// render(<App/>, document.getElementById('clockcontainer'));
 
 class Timer extends React.Component {
     constructor(props) {
@@ -30,72 +19,3 @@ class Timer extends React.Component {
 }
 
 render(<Timer/>, document.getElementById('clockcontainer'));
-// export default Timer;
-
-// var container = document.getElementById('clockcontainer');
-
-// var Clock = React.createClass({
-//     displayName: 'Clock',
-
-//     getInitialState: function() {
-//         return {
-//             hr: 0,
-//             min: 0,
-//             sec: 0,
-//         };
-//     },
-
-//     // componentDidMount is called when the component has been added to the DOM
-//     //
-//     // Use it ot initialize anything that's needed for the lifetime of the
-//     // element
-//     componentDidMount: function () {
-//         this._interval = setInterval(function () {
-//             // note that because we're calling `this.setState`,
-//             // the function must be "bound"
-//             if (this.state.sec < 60) {
-//                 this.setState({
-//                     sec: this.state.sec + 1
-//                 });
-//             } else {
-//                 if (this.state.min < 60) {
-//                     this.setState({
-//                         min: this.state.min + 1,
-//                         sec: 0
-//                     });
-//                 } else {
-//                     this.setState({
-//                         hr: this.state.hr + 1,
-//                         min: 0,
-//                         sec: 0
-//                     });
-//                 }
-//             }
-//         }.bind(this), 1000);
-//     },
-
-//     // componentWillMount is called when the component is about to be removed
-//     // from the DOM.
-//     //
-//     // It is importatn to dispose of anything that was created in
-//     // `componentDidMount`
-//     componentWillMount: function () {
-//         clearInterval(this._interval);
-//     },
-//     render: function () {
-//         return React.createElement(
-//             'p',
-//             {},
-//             this.state.hr,
-//             ':',
-//             this.state.min < 10 ? '0' + this.state.min.toString() : this.state.min,
-//             ':',
-//             this.state.sec < 10 ? '0' + this.state.sec.toString() : this.state.sec
-//         );
-//     }
-// });
-
-// ReactDOM.render(
-//     React.createElement(Clock, {}),
-//     container
-// );
