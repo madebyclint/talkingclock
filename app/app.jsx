2f5e09c331cb7231af9609c2c79df21055c6e2b4
Clint Bush <clint@madebyclint.com>
2017-03-11 10:40:58 -0800

    Update boilerplate.

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Route, Router, IndexRoute, hashHistory} from 'react-router'
+
+// import jquery just for foundation
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
 
 // Load Foundation
 $(document).foundation();
